a364ad4463a0f173b46ba317adef0fe7c46f0c4c
swood76 <35699946+swood76@users.noreply.github.com>
2018-09-10 20:58:50 -0400

    Update main.c

--- Assignment/main.c
+++ Assignment/main.c
@@ -6,7 +6,23 @@ int main()
     int num1;
     int num2;
     char Operator;
-    //implementation (function call) of math function which is defined in main.c file
+    //implementation (function call) of math function which is defined in main.c file; values inserted by user are passed
     math(num1, num2, Operator);
+    
+    /* if scanf was not used; an example of of each operation for each function call would be:
+    math(5, 7, '+');
+    math(5, 7, '-');
+    math(5, 7, '*');
+    math(2, 1, '/');
+    math(2, 1, '%');
+    math(2, 1,  '<');
+    math(2, 1, '>');
+    math(0, 1, '&');
+    math(0, 1, '|');
+    math(0, 1, '^');
+    math(0, 1, '~');
+    
+    
+    */
     return 0;
 }
