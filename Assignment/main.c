8f06c1be448fad9cec67326bb5c0606e21b51bce
swood76 <35699946+swood76@users.noreply.github.com>
2018-09-10 20:48:41 -0400

    Update main.c

--- Assignment/main.c
+++ Assignment/main.c
@@ -6,7 +6,7 @@ int main()
     int num1;
     int num2;
     char Operator;
-
+    //implementation (function call) of math function which is defined in main.c file
     math(num1, num2, Operator);
     return 0;
 }
